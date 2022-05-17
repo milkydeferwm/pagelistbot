1835300d2c83241169891c103d55d4c47952d326
milkydeferwm <97394099+milkydeferwm@users.noreply.github.com>
2022-05-17 21:43:32 +0800

    add parse

---
+++ parser/src/parse.rs
@@ -0,0 +1,28 @@
+#![cfg(feature="parse")]
+
+use nom::IResult;
+use nom::branch::alt;
+use nom::bytes::complete::tag_no_case;
+use nom::character::complete::space0;
+use nom::sequence::delimited;
+use nom_locate::LocatedSpan;
+
+use crate::ast::*;
+use crate::parser_types::*;
+
+#[cfg(test)]
+use pagelistbot_parser_test_macro::parse_test;
+
+pub(crate) type NomSpan<'a> = LocatedSpan<&'a str>;
+
+pub fn parse(input: NomSpan) -> IResult<NomSpan, Expr> {
+    todo!()
+}
+
+#[cfg_attr(
+    test,
+    parse_test(test_link_expr, "test/link_expr.in"),
+)]
+fn parse_link_expr(input: NomSpan) -> IResult<NomSpan, Expr> {
+    todo!();
+}
