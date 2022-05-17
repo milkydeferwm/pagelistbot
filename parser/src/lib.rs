51107742886a9240b5c5438a3d74833658db3124
milkydeferwm <97394099+milkydeferwm@users.noreply.github.com>
2022-05-17 21:44:13 +0800

    update lib

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,3 +2,15 @@ pub mod ast;
 
 #[cfg(feature="parse")]
 mod parser_types;
+#[cfg(feature="parse")]
+mod parse;
+
+/*
+#[cfg(feature="parse")]
+pub fn parse(input: &str) -> Result<(NomSpan, Expr), Error> {
+    use nom::error::Error;
+    use parse::NomSpan;
+
+    todo!()
+}
+*/
