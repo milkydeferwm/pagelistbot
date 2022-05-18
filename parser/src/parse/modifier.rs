fb01f24e035b0635f09ebd27395c1e723ff069b0
milkydeferwm <97394099+milkydeferwm@users.noreply.github.com>
2022-05-18 23:11:55 +0800

    implement resolve redirects modifier

--- parser/src/parse/modifier.rs
+++ parser/src/parse/modifier.rs
@@ -3,7 +3,8 @@
 use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::tag_no_case;
-use nom::character::complete::char;
+use nom::character::complete::{char, multispace0};
+use nom::combinator::opt;
 use nom::error::{ParseError, FromExternalError};
 use nom::multi::separated_list1;
 use nom::sequence::{delimited, preceded};
@@ -43,4 +44,34 @@ where
         )
     )(input)?;
     Ok((input, ModifierType::ResultLimit(limit)))
+}
+
+/// Parse a ResultLimit modifier. Assume no leading or trailing spaces.
+/// 
+/// Must be written as
+/// * `resolve()`; or
+/// * `resolve`
+/// 
+/// The brackets are optional. There might be spaces between tokens. 
+#[cfg_attr(
+    test,
+    parse_test(test_resolve_redirects, "test/resolve_redirects.in"),
+)]
+fn parse_resolve_redirects<'a, E>(input: StrSpan) -> IResult<StrSpan, ModifierType>
+where
+    E: ParseError<StrSpan<'a>>
+{
+    let (input, _) = preceded(
+        tag_no_case("resolve"),
+        opt(
+            ws(
+                delimited(
+                    char('('),
+                    multispace0,
+                    char(')')
+                )
+            )
+        )
+    )(input)?;
+    Ok((input, ModifierType::ResolveRedirects))
 }
