97ced5233b23bd54bcfc5a77f456f024f7570fce
milkydeferwm <97394099+milkydeferwm@users.noreply.github.com>
2022-05-19 11:07:43 +0800

    expose parse method

--- parser/src/parse/mod.rs
+++ parser/src/parse/mod.rs
@@ -10,11 +10,11 @@ mod util;
 
 type StrSpan<'a> = nom_locate::LocatedSpan<&'a str>;
 
-pub fn parse<'a, E>(input: &str) -> Result<crate::ast::Expr, E>
+pub fn parse<'a, E: 'a>(input: &'a str) -> Result<crate::ast::Expr, E>
 where
     E: nom::error::ParseError<StrSpan<'a>> + nom::error::FromExternalError<StrSpan<'a>, std::num::ParseIntError>
 {
     use nom::Finish;
 
-    expr::parse(StrSpan::new(input)).finish().map(|(_, res)| res)
+    expr::parse::<E>(StrSpan::new(input)).finish().map(|(_, res)| res)
 }
