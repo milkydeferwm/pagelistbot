66ff15bc7cb1734cbc5f46a743409680f228ca36
MilkyDefer <milkydefer@gmail.com>
2023-01-31 03:31:40 +0000

    overhaul of provider interface

--- lib/provider/src/lib.rs
+++ lib/provider/src/lib.rs
@@ -1,8 +1,12 @@
 //! Traits and common data structures for data provider.
 
+#![allow(incomplete_features)]
+#![feature(is_some_and)]
+#![feature(return_position_impl_trait_in_trait)]
+
 pub mod core;
 #[cfg(feature = "api")]
 pub mod api;
 
 // re-exports of core traits and types
-pub use crate::core::{DataProvider, PageInfo, PagePair};
+pub use crate::core::{DataProvider, PageInfo, PageInfoError, Pair};
