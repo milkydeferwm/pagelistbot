2b32e455bd39b5ed8ac1fddb898f3fc44618d7f4
MilkyDefer <milkydefer@gmail.com>
2023-03-17 08:28:14 +0000

    fix doc

--- lib/solver/src/lib.rs
+++ lib/solver/src/lib.rs
@@ -4,7 +4,7 @@
 //! also provides other crates a layer of abstract, so that other parts 
 //! of the solver can easily switch between implementations.
 //! 
-//! A built-in solver, `RecursiveTreeSolver`, is available through the `recursive-tree` feature gate.
+//! A built-in solver, `TreeSolver`, is available through the `tree-future` feature gate.
 
 #![allow(incomplete_features)]
 #![feature(async_fn_in_trait, is_some_and, type_alias_impl_trait)]
