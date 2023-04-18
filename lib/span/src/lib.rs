b8f2e4d5dc4e5b38c30bcc8dc4dce6c9bb590c9b
MilkyDefer <milkydefer@gmail.com>
2023-04-18 10:29:43 +0000

    add `span` crate

---
+++ lib/span/src/lib.rs
@@ -0,0 +1,42 @@
+#![no_std]
+
+use core::ops::Range;
+#[cfg(feature = "use_serde")]
+use serde::{Serialize, Deserialize};
+
+/// Struct to store a span.
+/// A span is composed from a start offset and span length.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "use_serde", derive(Serialize, Deserialize))]
+pub struct Span {
+    offset: usize,
+    length: usize,
+}
+
+impl Span {
+    /// Construct a `Span` from raw offset and length value
+    pub fn new(offset: usize, length: usize) -> Self {
+        Self {
+            offset,
+            length,
+        }
+    }
+}
+
+impl From<Range<usize>> for Span {
+    fn from(value: Range<usize>) -> Self {
+        Self {
+            offset: value.start,
+            length: value.end - value.start,
+        }
+    }
+}
+
+impl From<Span> for Range<usize> {
+    fn from(value: Span) -> Self {
+        Self {
+            start: value.offset,
+            end: value.offset + value.length,
+        }
+    }
+}
