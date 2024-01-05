48d9c0525a3ba74073487f36888f1110e4322bda
MilkyDefer <milkydefer@gmail.com>
2024-01-05 16:15:54 +0000

    env: allow more environment variables

--- lib/env/src/lib.rs
+++ lib/env/src/lib.rs
@@ -13,10 +13,18 @@ pub fn pagelistbot_home() -> PathBuf {
 
 #[inline]
 pub fn pagelistbot_bin() -> PathBuf {
-    pagelistbot_home().join("bin")
+    if let Ok(x) = env::var("PAGELISTBOT_BIN") {
+        PathBuf::from(x)
+    } else {
+        pagelistbot_home().join("bin")
+    }
 }
 
 #[inline]
 pub fn pagelistbot_log() -> PathBuf {
-    pagelistbot_home().join("logs")
+    if let Ok(x) = env::var("PAGELISTBOT_LOG") {
+        PathBuf::from(x)
+    } else {
+        pagelistbot_home().join("logs")
+    }
 }
